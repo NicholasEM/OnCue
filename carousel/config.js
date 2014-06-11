fc9dbfe1970e136d8bd37665a033c858ddfd18fb
NicholasEM <livefyre@livefyres-MacBook-Air.local>
2014-06-10 17:46:32 -0700

    Media carousel with right logos and such.  Still needs autorefresh moved in.

--- carousel/config.js
+++ carousel/config.js
@@ -6,39 +6,27 @@ var SLIDE_CONFIG = {
     carouselInterval: 10000,
 
     // The time (in milliseconds) between tweets fading in and out
-    feedScrollerInterval: 10000,
+    feedScrollerInterval: 100000,
 
     // The number of rotations before the carousel reloads
     reloadCycle: 0,
 
     // Collections info
-    counterWall: {
-        network: "strategy-prod.fyre.co",
-        siteId: "340628",
-        articleId: "custom-1395010000184"
-    },
     listFeed1: {
-        network: "strategy-prod.fyre.co",
-        siteId: "340628",
-        articleId: 'custom-1395009388264',
+        network: "reveredigital.fyre.co",
+        siteId: "350333",
+        articleId: 'custom-1400543277205',
         initial: 10,
         showMore: 0
     },
-    map: {
-        network: "strategy-prod.fyre.co",
-        siteId: "340628",
-        articleId: "custom-1395009388264",
-        cloudmadeStyleId: 56565,
-        leafletMapOptions: {
-            center: [37.7825471,-122.3978904],
-            styles: 'dark',
-            zoom: 18
-        }
-    },
-    get: function (key) {
-         return this[key];
-    },
-    set: function (key, value) {
-        this[key] = value;
-    }
+
+
+//listFeed1: {
+//    network: "strategy-prod.fyre.co",
+//    siteId: "340628",
+//    articleId: 'custom-1395009388264',
+//    initial: 10,
+//    showMore: 0
+//},
+
 };
---
+++ carousel/js/main.js
@@ -0,0 +1,90 @@
+
+(function(){
+    var stage, textStage, form, input;
+    var circles, textPixels, textFormed;
+    var offsetX, offsetY, text;
+    var colors = ['#00637d', '#c65918', '#d1ceca'];
+
+    function init() {
+        initStages();
+        initCircles();
+        animate();
+        addListeners();
+    }
+
+    // Init Canvas
+    function initStages() {
+        offsetX = (window.innerWidth-600)/2;
+        offsetY = (window.innerHeight-300)/2;
+        textStage = new createjs.Stage("text");
+        textStage.canvas.width = 600;
+        textStage.canvas.height = 200;
+
+        stage = new createjs.Stage("stage");
+        stage.canvas.width = window.innerWidth;
+        stage.canvas.height = window.innerHeight;
+    }
+
+
+
+    function initCircles() {
+        circles = [];
+        for(var i=0; i<40; i++) {
+            var circle = new createjs.Shape();
+            var r = 4;
+            var x = window.innerWidth*Math.random();
+            var y = window.innerHeight*Math.random();
+            var color = colors[Math.floor(i%colors.length)];
+            var alpha = 0.01 + Math.random()*0.1;
+            circle.alpha = alpha;
+            circle.radius = r;
+            circle.graphics.beginFill(color).drawCircle(0, 0, r);
+            circle.x = x;
+            circle.y = y;
+            circles.push(circle);
+            stage.addChild(circle);
+            circle.movement = 'float';
+            tweenCircle(circle);
+        }
+    }
+
+
+    // animating circles
+    function animate() {
+        stage.update();
+        requestAnimationFrame(animate);
+    }
+
+    function tweenCircle(c, dir) {
+        if(c.tween) c.tween.kill();
+        if(dir == 'in') {
+            c.tween = TweenLite.to(c, 0.4, {x: c.originX, y: c.originY, ease:Quad.easeInOut, alpha: .5, radius: 5, scaleX: 0.4, scaleY: 0.4, onComplete: function() {
+                c.movement = 'jiggle';
+                tweenCircle(c);
+            }});
+        } else if(dir == 'out') {
+            c.tween = TweenLite.to(c, 0.8, {x: window.innerWidth*Math.random(), y: window.innerHeight*Math.random(), ease:Quad.easeInOut, alpha: 0.2 + Math.random()*0.3, scaleX: 1, scaleY: 1, onComplete: function() {
+                c.movement = 'float';
+                tweenCircle(c);
+            }});
+        } else {
+            if(c.movement == 'float') {
+                c.tween = TweenLite.to(c, 10 + Math.random()*3.5, {x: c.x + -100+Math.random()*200, y: c.y + -100+Math.random()*200, ease:Quad.easeInOut, alpha: 0.2 + Math.random()*0.15,
+                    onComplete: function() {
+                        tweenCircle(c);
+                    }});
+            } else {
+                c.tween = TweenLite.to(c, 0.05, {x: c.originX + Math.random()*3, y: c.originY + Math.random()*3, ease:Quad.easeInOut,
+                    onComplete: function() {
+                        tweenCircle(c);
+                    }});
+            }
+        }
+    }
+
+
+
+
+
+    window.onload = function() { init() };
+})();
